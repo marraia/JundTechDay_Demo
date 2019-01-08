461fa6d53c475f4060a16cd3d6bd678553aeb34a
Fernando Mendes <fernando.abreu@gmail.com>
2019-01-08 15:18:41 -0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 echo 'Acessando pasta para executar build'
 cd '/home/marraia/myagent/_work/1/s'
 echo 'Executar Docker Compose'
-ssh -t sudo docker-compose up -d
+ssh -t marraia sudo docker-compose up -d
