93ccb352e727fc3f1804e53771a38f85b60182e3
Fernando Mendes <fernando.abreu@gmail.com>
2019-01-08 15:57:10 -0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+echo "AGENT_BUILDDIRECTORY"
+ls -1 $AGENT_BUILDDIRECTORY
+echo "SYSTEM_HOSTTYPE is $SYSTEM_HOSTTYPE"
 echo 'Acessando pasta para executar build'
 cd '/home/marraia/myagent/_work/1/s'
 echo 'Executar Docker Compose'
-docker-compose up -d
+/usr/local/bin/docker-compose up -d
